7e94c482b20437140f8b3c455bea3c13e18439ff
NPException <npexception42@gmail.com>
2023-10-03 20:26:35 +0200

    make .bat build scripts more similar to .ps1 script

--- build+run.bat
+++ build+run.bat
@@ -1,5 +1,6 @@
 @echo OFF
 call build.bat
+for %%I in (.) do set CurrDirName=%%~nxI
 if NOT %ERRORLEVEL% == 0 exit /b %ERRORLEVEL%
 echo Starting Simulator ...
-%PLAYDATE_SDK_PATH%\bin\PlaydateSimulator.exe .\builds\game.pdx
+%PLAYDATE_SDK_PATH%\bin\PlaydateSimulator.exe ".\builds\%CurrDirName%.pdx"
--- build.bat
+++ build.bat
@@ -1,5 +1,6 @@
 @echo OFF
+for %%I in (.) do set CurrDirName=%%~nxI
 rmdir /S /Q builds
 mkdir builds
-%PLAYDATE_SDK_PATH%\bin\pdc --verbose source builds\game
+%PLAYDATE_SDK_PATH%\bin\pdc --verbose source ".\builds\%CurrDirName%"
 exit /b %ERRORLEVEL%
