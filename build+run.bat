a7fc19373f6e1d31e3e678ba3fec577ec2645e25
NPException <npexception42@gmail.com>
2023-10-14 22:05:16 +0200

    remove batch files

--- build+run.bat
+++
@@ -1,6 +0,0 @@
-@echo OFF
-call build.bat
-for %%I in (.) do set CurrDirName=%%~nxI
-if NOT %ERRORLEVEL% == 0 exit /b %ERRORLEVEL%
-echo Starting Simulator ...
-%PLAYDATE_SDK_PATH%\bin\PlaydateSimulator.exe ".\builds\%CurrDirName%.pdx"
--- build.bat
+++
@@ -1,6 +0,0 @@
-@echo OFF
-for %%I in (.) do set CurrDirName=%%~nxI
-rmdir /S /Q builds
-mkdir builds
-%PLAYDATE_SDK_PATH%\bin\pdc --verbose source ".\builds\%CurrDirName%"
-exit /b %ERRORLEVEL%
